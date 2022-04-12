66e97147298fc848e81574aae61c9dd0d08897a0
Mehrdad <rashidi.mehrdad@gmail.com>
2022-04-12 21:21:16 +0430

    first

--- DateAndTimeUtillity.py
+++ DateAndTimeUtillity.py
@@ -5,6 +5,7 @@ def convertJalaliToGregorian(date):
     year = int(date[0:4])
     month = int(date[4:6])
     day = int(date[6:8])
+    print(year)
     gregorian_date = jdatetime.date(year, month, day).togregorian()
     return gregorian_date
 
